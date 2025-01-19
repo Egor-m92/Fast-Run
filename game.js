ae1ed2bd6aa68ccfca4709ac660d1a7e449ae4b1
Egor-m92 <mgldn.by@gmail.com>
2025-01-19 23:25:10 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
-let width = 480;
-let height = 640;
+const height = window.visualViewport ? 
+window.visualViewport.height : window.innerHeight;
+const width = window.visualViewport ?
+Math.min(window.visualViewport.width, height * 0.6) : 
+Math.min(window.innerWidth, height * 0.6);
+let gameHeight = 640;
+let gameWidth = 400 + width / height;
 
-let pjs = new PointJS(width, height, {backgroundColor: '#696969'});
+let pjs = new PointJS(gameWidth, gameHeight, {backgroundColor: '#696969'});
 
 let game = pjs.game;
 let p = pjs.vector.point;
@@ -12,8 +17,8 @@ let touch = pjs.touchControl;
 mouse.initControl();
 touch.initControl();
 
-let speed = 5;
-let rivalCarSpeed = 7;
+let speed = 8;
+let rivalCarSpeed = 12;
 let wRoadL = width / 5;
 let wRoadR = width / 1.5;
 let countTimer = 0;
@@ -21,13 +26,18 @@ let time = 3;
 let startTime = setInterval(() => time--, 1000);
 let score = setInterval(() => score++, 5000);
 
+let music = new Audio('/machine_stop_2.wav');
+let carSound = new Audio('/car_sound.wav');
+
 let bloksLeft = [];
 let bloksRight = [];
 let blockCenter = [];
 let rCar = [];
-let timer = pjs.OOP.newTimer(1500, function() {carSpawner();});
+let shlagbaum = [];
+let rivalCarSpawer = pjs.OOP.newTimer(1200, function() {carSpawner();});
 let sw = 10, sh = 100;
 
+// функция отрисовки дорожной разметки
 function roadDraw() {  
     for (let i in bloksLeft) {
         bloksLeft[i].draw();
@@ -37,7 +47,7 @@ function roadDraw() {
         blockCenter[a].draw();
     }
 }
-
+// функция движения дорожной разметки
 function roadMove() {
     for (let i in blockCenter) {
         blockCenter[i].move(p(0, speed));
@@ -56,19 +66,21 @@ function roadMove() {
         }
     } 
 }
-
+// функция движения машины игрока, обработка событий сенсорного экрана и мыши
 function carMove() {
     if (mouse.isDown('LEFT') || touch.isDown()) {
         let pos = mouse.getPosition();
         if (pos.x > width / 2) {
-            car.setPosition(p(width / 1.5, car.y), speed, 0);
+            car.moveTime(p(width / 1.5, car.y), 3);
+            //car.setPosition(p(width / 1.5, car.y), speed, 0);
         }
         else {
-            car.setPosition(p(width / 5, car.y), speed, 0);
+            car.moveTime(p(width / 5, car.y), 3);
+            //car.setPosition(p(width / 5, car.y), speed, 0);
     }
     }
 }
-
+// спаун машин соперников
 function carSpawner() {
     countTimer = math.random(1, 4);
     if (countTimer % 2 == 0) {
@@ -94,7 +106,7 @@ function carSpawner() {
     }
     
   }
-
+// функция отрисовки машин проивника из массива
 function rivalCar() {
     for (let i in rCar) {
         rCar[i].drawStaticBox();
@@ -102,16 +114,22 @@ function rivalCar() {
         
     }        
 }
-
+// функция движения машин противника, обработка коллизий
 function rivalCarMove() {
     for (let i in rCar) {
         rCar[i].move(p(0, rivalCarSpeed));
+        if (rCar[i].y - rCar.h > height) {
+            pjs.OOP.delObject(rCar, rCar[i]);
+        }
         if(car.isStaticIntersect(rCar[i].getStaticBox())) {
             pjs.OOP.delObject(rCar, rCar[i]);
             score = 0;
             time = 3;
+            carSound.pause();
+            music.play();
             game.setLoop('startGame');
         }
+        
     }
 }
 
@@ -147,20 +165,19 @@ for (let x = 0; x < 4; x++) {
     strokeWidth: 1,
     strokeColor: 'black'}))
 }
-
+// создание машины игрока
 let car = game.newRectObject({ 
     x : width / 1.5, 
     y : height - 150, 
     w : 70, 
-    h : 100, 
+    h : 100,
     fillColor : "#228B22",
     strokeWidth: 1,
     strokeColor: 'black' 
   });
-  car.drawStaticBox();
 
   let text = game.newTextObject(   { 
-    x : width / 10, 
+    x : width / 15, 
     y : 30, 
     text : "Расстояние: ", 
     size : 30, 
@@ -176,6 +193,7 @@ let car = game.newRectObject({
     padding : 10, 
     color : "#000000" 
   });
+
   let warningText = game.newTextObject(   { 
     x : width / 3.5, 
     y : height / 3.5, 
@@ -184,9 +202,9 @@ let car = game.newRectObject({
     padding : 10, 
     color : "#000000" 
   });
-
+// отсчет обратного времени
 let rever = pjs.OOP.newRever(0, 100000, 1);
-
+// функция сартового меню
 function startGameMenu() {
     gameName.draw();
     warningText.draw();
@@ -197,21 +215,23 @@ function startGameMenu() {
         color : "black", 
         size : 100
         });
+
     if (time === 0) {
         game.setLoop('myGame');
     }
 }
-
+// игровой цикл Меню
   game.newLoop('startGame', function() {
     startGameMenu();
   }) 
   
-
+// основной игровой цикл
 game.newLoop('myGame', function(){
-
+    music.pause();
+    carSound.play();
     roadMove();
     roadDraw();
-    timer.restart();
+    rivalCarSpawer.restart();
     rivalCarMove();
     rivalCar();
     
